191231793593317dc640f29224d7cba5b42cc87b
TheSkyentist <raphael.hviding@gmail.com>
2024-04-12 10:59:30 -0700

    Added to GameBoard constructor

--- src/gameBoard.lua
+++ src/gameBoard.lua
@@ -1,13 +1,45 @@
 -- create GameBoard object
 GameBoard = Object:extend()
 
--- universe constructor
+-- GameBoard constructor
 function GameBoard:new()
+
+    -- Create Table of Galaxies
     self.galaxies = {}
 
-    print(self.galaxies[1])
-end
+    -- Open a file and get the lines
+    local lines = {}
+    for line in io.lines("assets/galaxies.tsv") do
+        table.insert(lines, line)
+    end
 
-function playerMovement()
-    
+    -- Create Table of Connections
+    self.connections = {}
+    for i = 1,#lines do
+        local row = {}
+        for j = 1,#lines do 
+            table.insert(row, 0)
+        end
+        table.insert(self.connections, row)
+    end
+
+    -- Iterate over lines
+    for i = 1,#lines do 
+
+        -- Split line on tabs
+        local words = {}
+        for word in lines[i]:gmatch("[^\t]+") do
+            table.insert(words, word)
+        end
+
+        -- Create a new galaxy
+        local galaxy = Galaxy(words[1])
+        table.insert(self.galaxies, galaxy)
+
+        -- Add connections
+        for j = 2,#words do
+            self.connections[i][j-1] = 1
+            self.connections[j-1][i] = 1
+        end
+    end
 end
